30dd05e2b52131083e62528b1d47a4b5d7fa463e
Guenrick <147289316+Guenrick@users.noreply.github.com>
2026-05-18 00:05:03 -0300

    Refatorando para garantir canonicidade

--- sprint02/src/system.cpp
+++ sprint02/src/system.cpp
@@ -7,7 +7,7 @@ System::System(const System& obj) : value(obj.value) {}
 System::~System() {}
 
 System& System::operator=(const System& obj) {
-    if (this == &obj) return *this; // Proteção contra auto-atribuição (ex: s1 = s1)
+    if (this == &obj) return *this; 
     value = obj.value;
     return *this;
 }
