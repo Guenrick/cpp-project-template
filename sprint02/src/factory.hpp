cb9e540d7b383353cbb88d9cff828f81907b5234
DavidSouza99 <daviddasou@gmail.com>
2026-06-09 09:11:31 -0300

    docs: adiciona comentarios Doxygen na factory

--- sprint02/src/factory.hpp
+++ sprint02/src/factory.hpp
@@ -4,8 +4,25 @@
 #include "system.hpp"
 #include "model.hpp"
 
-/****************** Factory ******************/
+/**
+ * @file factory.hpp
+ * @brief Funcoes de fabrica para criacao de objetos do MyVENSIM.
+ *
+ * Expoe apenas as interfaces publicas (System e Model),
+ * ocultando completamente as implementacoes concretas (SystemImpl, ModelImpl).
+ */
+
+/**
+ * @brief Cria um novo objeto System.
+ * @param value Valor inicial do sistema (padrao: 0.0).
+ * @return Ponteiro para a interface System. O chamador e responsavel por deletar o objeto.
+ */
 System* createSystem(double value = 0.0);
+
+/**
+ * @brief Cria um novo objeto Model.
+ * @return Ponteiro para a interface Model. O chamador e responsavel por deletar o objeto.
+ */
 Model* createModel();
 
 #endif // FACTORY_HPP
